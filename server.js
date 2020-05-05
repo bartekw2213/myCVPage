fdcd244e6bb6f29c9eacf62cfee83f01822b14f1
Bartek Wilczyński <bartekw2213@gmail.com>
2020-05-05 16:10:42 +0200

    Serving index.html

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.json({ extended: true }));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('./client/build'));
 }
 
-app.get('/', (req, res) => res.send('Server is up and running'));
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+);
 app.use('/email', require('./routes/email'));
 
 const PORT = process.env.PORT || 5000;
